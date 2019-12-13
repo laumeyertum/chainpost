ee29aec109d92d8bcd49ddbb77e08cca3aa25859
leon <leon.laumeyer@tum.de>
2019-12-13 17:04:54 +0900

    added utils and token

--- contracts/TokenEconomy.sol
+++ contracts/TokenEconomy.sol
@@ -1,13 +1,24 @@
 pragma solidity ^0.5.8;
 
+import "./utils/Ownable.sol";
+import "./utils/SafeMath.sol";
+import "./token/SafeERC20.sol";
+
 interface token {
     function transfer(address receiver, uint amount) external;
 }
 
-contract TokenEconomy {
-    token private MemeCoin;
+contract TokenEconomy is Ownable{
+
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 private _token;
 
     constructor(address _tokenAddress) public {
-        MemeCoin = token(_tokenAddress);
+        MemeCoin = IERC20(_tokenAddress);
     }
+
+
+
 }
