85736ce7bc0597a6aaebdba02268b2cf92dcd540
rutger <r.g.kerkhof@student.utwente.nl>
2019-12-14 23:53:48 +0900

    Added functionality
    Should be able to post & up/downvote, only needs implementation of loggin for username

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,12 +2,62 @@ function getBase64 (file,callback) {
     const reader = new FileReader();
     reader.addEventListener('load', () => callback(reader.result));
     reader.readAsDataURL(file);
+let base64;
+
+function changeInputField() {
+    let selected = $('#typeSelection input:radio:checked').val();
+    let textinput = document.getElementById("inputTextdiv");
+    let imageinput = document.getElementById("inputImagediv");
+    if (selected === "text") {
+        textinput.style.display = "block";
+        imageinput.style.display = "none";
+    } else {
+        textinput.style.display = "none";
+        imageinput.style.display = "block";
+    }
+}
+
+function encodeImageFileAsURL() {
+    let filesSelected = document.getElementById("inputFileToLoad").files;
+    if (filesSelected.length === 0) {
+        let fileToLoad = filesSelected[0];
+        let fileReader = new FileReader();
+        fileReader.onload = function(fileLoadedEvent) {
+            let srcData = fileLoadedEvent.target.result; // <--- data: base64
+            let newImage = document.createElement('img');
+            newImage.src = srcData.toString();
+            base64 = newImage.outerHTML;
+        };
+        fileReader.readAsDataURL(fileToLoad);
+    }
 }
 
 getBase64(fileObjectFromInput, function(base64Data){
     console.log("base 64 of file is",base64Data);//here you can have your code which uses base64 for its operation,//file to base64 by oneshubh
+$('.postForm').on('submit', function () {
+    let title = $('#postTitle').val();
+    let username = localStorage.getItem("username");
+    if (username && title && (document.getElementById("postText").value.length > 0 || document.getElementById("upload").val())) {
+        let type = $('#typeSelection input:radio:checked').val();
+        encodeImageFileAsURL();
+        let content;
+        if (type === "image") {
+            content = base64;
+        } else if (type === "text") {
+            content = document.getElementById("postText").value;
+        } else {
+            console.log('No picture or text selected');
+            return;
+        }
+        base64 = null;
+        return [title, content, type, username];
+    } else {
+        $("loginPopup").show();
+    }
 });
 
+
+/*
 $(document).ready(async function (e) {
     $('#postForm').on('submit',(async function(e) {
         e.preventDefault();
@@ -33,4 +83,6 @@ $(document).ready(async function (e) {
             }
         });
     }));
-});
+});});
+
+ */
