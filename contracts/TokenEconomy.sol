11fdb17a7abb0abdaa78771a0d7ae866ac15c8ce
leon <leon.laumeyer@tum.de>
2019-12-17 21:46:21 +0900

    better version of tokeneconomy part one

--- contracts/TokenEconomy.sol
+++ contracts/TokenEconomy.sol
@@ -5,7 +5,7 @@ import "./utils/SafeMath.sol";
 import "./token/SafeERC20.sol";
 import "./token/IERC20.sol";
 
-contract TokenEconomy is Ownable{
+contract TokenEconomy is Ownable {
 
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
@@ -15,15 +15,24 @@ contract TokenEconomy is Ownable{
     address payable private _wallet;
     uint private _likeWorth = 10;
 
-    struct Like{
+    struct Like {
         uint postId;
         address poster;
         bool type;
     }
 
-    Like[] likeList;
+    Like[] private likeList;
+    uint[] private postLikeList;
+    mapping (uint => PostLike) private likeMapping;
 
-    constructor(address _tokenAddress,address wallet,  uint rate) public {
+    struct PostLike{
+        uint PostId;
+        address poster;
+        uint amountLike;
+        uint amountDisLike;
+    }
+
+    constructor(address _tokenAddress, address wallet, uint rate) public {
         require(rate > 0, "TokenEconomy: rate is 0");
         require(wallet != address(0), "TokenEconomy: wallet is the zero address");
         require(address(token) != address(0), "TokenEconomy: token is the zero address");
@@ -33,14 +42,15 @@ contract TokenEconomy is Ownable{
         _rate = rate;
     }
 
-    function () external payable{
+    function() external payable {
         buyMemeCoin(msg.sender);
     }
 
     function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view {
         require(beneficiary != address(0), "TokenEconomy: beneficiary is the zero address");
         require(weiAmount != 0, "TokenEconomy: weiAmount is 0");
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        this;
+        // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
     }
 
     function buyMemeCoin(address beneficiary) public payable {
@@ -69,38 +79,73 @@ contract TokenEconomy is Ownable{
 
     function giveLike(address to, uint postId){
         like = Like(msg.sender, owner(), postId, true);
-        likeList.push(Like(postId,to, true));
+        likeList.push(Like(postId, to, true));
     }
 
-    function getLikeWorth() external view returns(uint){
+    function giveLike(address to, uint postId){
+        _token.safeTransferFrom(msg.sender, owner(), _likeWorth);
+        if(likeMapping[postId].postId == 0){
+            PostLike(postId,to,1,0);
+            postLikeList.push(postId);
+        }else{
+            likeMapping[postId].amountLike += 1;
+        }
+    }
+
+    function getLikeWorth() external view returns (uint){
         return _likeWorth;
     }
 
-    function setLikeWorth(uint likeWorth) external onlyOwner{
+    function setLikeWorth(uint likeWorth) external onlyOwner {
         _likeWorth = likeWorth;
     }
 
-    function giveDislike(address to, uint postId)external{
+    function giveDislike(address to, uint postId) external {
         like = Like(msg.sender, owner(), postId, true);
-        likeList.push(Like(postId,to, false));
+        likeList.push(Like(postId, to, false));
     }
 
-    function giveGift(address to, uint amount ){
+    function giveDislike(address to, uint postId) external {
+        _token.safeTransferFrom(msg.sender, owner(), _likeWorth);
+        if(likeMapping[postId].postId == 0){
+            PostLike(postId,to,0,1);
+            postLikeList.push(postId);
+        }else{
+            likeMapping[postId].amountDisLike += 1;
+        }
+    }
+
+    function giveGift(address to, uint amount){
         return _token.safeTransferFrom(msg.sender, to, amount);
     }
 
-    function rewardForLikes(uint[] memory postId,address[] reporter, address [][] confirmer ){
-        for (uint i=0; i<likeList.length; i++) {
-            for (uint i=0; i<postId.length; i++) {
-                if(likeList[i].postId == postId[i]){
-
+    function rewardForLikes(uint[] memory postId, address[] reporter, address [][] confirmer){
+        for (uint i = 0; i < likeList.length; i++) {
+            if (!_reportExist(likeList[i], postId, reporter, confirmer)) {
+                if (likeList[i].type) {
+                    _token.safeTransferFrom(owner(), likeList[i].poster, _likeWorth);
                 }
-            }
-            if(likeList[i].type){
-                _token.safeTransferFrom(owner(),likeList[i].poster,_likeWorth);
             }
         }
     }
 
-     function _reportExist()
+    function _reportExist(Like like, uint[] memory postId, address[] reporter, address [][] confirmer) internal returns (bool){
+        for (uint i = 0; i < postId.length; i++) {
+            if (like.postId == postId[i]) {
+
+            }
+        }
+        return false;
+    }
+
+    function rewardForLikes(uint[] memory postId, address[] reporter, address [][] confirmer){
+        for (uint i = 0; i < postLikeList.length; i++) {
+            if (!_reportExist(postLikeList[i], postId, reporter, confirmer)) {
+                if (postLikeList[i].type) {
+                    _token.safeTransferFrom(owner(), postLikeList[i].poster, _likeWorth);
+                }
+            }
+        }
+    }
+
 }
