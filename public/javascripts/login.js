7845f5d7dd611c7cb104d74f3494f986e17b4f8b
lchauvet <lchauvet@postech.ac.kr>
2019-12-18 22:05:10 +0900

    login should work

---
+++ public/javascripts/login.js
@@ -0,0 +1,25 @@
+function sendUserInfo(){
+    console.log("senduserinfo");
+    let username = $('#enteredUsername').val();
+    let password = $('#passphrase').val();
+    console.log(username);
+
+    let login =
+        {
+            username: username,
+            password: password,
+        };
+
+    $.ajax({
+        type: 'POST',
+        url: "register/loginForm",
+        data: login,
+        dataType: 'json',
+        success: afterLogin(username)
+    });
+}
+
+function afterLogin(username){
+    $("#loginPopup").hide();
+    sessionStorage.username = username;
+}
